7581302824f06aa9d0de2c1420ff247b5be308ce
Alex Nicholas <alex.nicholas@bbc.co.uk>
2022-02-01 19:20:42 +0000

    added option for first topic

--- freelancer/quizCheck.js
+++ freelancer/quizCheck.js
@@ -2,42 +2,42 @@ let totalScore = 0;
 let topicScore = 0;
 let topics = ["celebrities", "conservation", "geography", "history", "law", "politics", "sport"];
 
-mydata.jillsquiz.forEach((element, index) => {
-  let topic = element.topic;
-  let mytemplate = `<iframe width="280" height="157.5" src='${element.quizquestions[0].video_source}' title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-  let numberOfQuestions = element.quizquestions.length;
-  for (let i = 0; i < element.quizquestions.length; i++) {
-    let ques = element.quizquestions[i].question;
+// mydata.jillsquiz.forEach((element, index) => {
+//   let topic = element.topic;
+//   let mytemplate = `<iframe width="280" height="157.5" src='${element.quizquestions[0].video_source}' title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+//   let numberOfQuestions = element.quizquestions.length;
+//   for (let i = 0; i < element.quizquestions.length; i++) {
+//     let ques = element.quizquestions[i].question;
 
-    let ans = element.quizquestions[i].correct_answer;
-    let ans1 = element.quizquestions[i].answers[0];
-    let ans2 = element.quizquestions[i].answers[1];
-    let ans3 = element.quizquestions[i].answers[2];
+//     let ans = element.quizquestions[i].correct_answer;
+//     let ans1 = element.quizquestions[i].answers[0];
+//     let ans2 = element.quizquestions[i].answers[1];
+//     let ans3 = element.quizquestions[i].answers[2];
 
-    mytemplate =
-      mytemplate +
-      `<div class="col-lg-12 ml-auto">
-        <p class="pre-wrap lead">Question - ${ques}</p>
-        <p class="pre-wrap lead">
-            <label for="${topic}">Choose an answer:</label>
-            <select name="${topic}" id="${topic}-option-${i}">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
-            <ol>
-                <li>${ans1}</li>
-                <li>${ans2}</li>
-                <li>${ans3}</li>
-            </ol>
-        </p>
-        <br/><br/>
-      </div>
-     `;
-  }
-  mytemplate = mytemplate + `<button class="navbar-toggler navbar-toggler-right font-weight-bold bg-primary text-white rounded" id="check-answer-button-${topic}" onclick="checkAllAnswers('${topic}', '${numberOfQuestions}')">Check all answers</button>`;
-  document.getElementById(index.toString()).innerHTML = mytemplate;
-});
+//     mytemplate =
+//       mytemplate +
+//       `<div class="col-lg-12 ml-auto">
+//         <p class="pre-wrap lead">Question - ${ques}</p>
+//         <p class="pre-wrap lead">
+//             <label for="${topic}">Choose an answer:</label>
+//             <select name="${topic}" id="${topic}-option-${i}">
+//               <option value="1">1</option>
+//               <option value="2">2</option>
+//               <option value="3">3</option>
+//             </select>
+//             <ol>
+//                 <li>${ans1}</li>
+//                 <li>${ans2}</li>
+//                 <li>${ans3}</li>
+//             </ol>
+//         </p>
+//         <br/><br/>
+//       </div>
+//      `;
+//   }
+//   mytemplate = mytemplate + `<button class="navbar-toggler navbar-toggler-right font-weight-bold bg-primary text-white rounded" id="check-answer-button-${topic}" onclick="checkAllAnswers('${topic}', '${numberOfQuestions}')">Check all answers</button>`;
+//   document.getElementById(index.toString()).innerHTML = mytemplate;
+// });
 
 function checkAllAnswers(thistopic, numberOfQuestions) {
     topicScore = 0;
@@ -74,7 +74,120 @@ function check(thistopic, correctanswer) {
     alert("Not Correct");
   }
 }
-
+function showQuestions(option) {
+    alert(`Your choice is ${option}`);
+    if (option === 'videos') {
+        mydata.jillsquiz.forEach((element, index) => {
+            let topic = element.topic;
+            let mytemplate = `<iframe width="280" height="157.5" src='${element.quizquestions[0].video_source}' title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            let numberOfQuestions = element.quizquestions.length;
+            for (let i = 0; i < element.quizquestions.length; i++) {
+                let ques = element.quizquestions[i].question;
+            
+                let ans = element.quizquestions[i].correct_answer;
+                let ans1 = element.quizquestions[i].answers[0];
+                let ans2 = element.quizquestions[i].answers[1];
+            
+                mytemplate =
+                    mytemplate +
+                    `<div class="col-lg-12 ml-auto">
+                    <p class="pre-wrap lead">Question - ${ques}</p>
+                    <p class="pre-wrap lead">
+                        <label for="${topic}">Choose an answer:</label>
+                        <select name="${topic}" id="${topic}-option-${i}">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                        <ol>
+                            <li>${ans1}</li>
+                            <li>${ans2}</li>
+                        </ol>
+                    </p>
+                    <br/><br/>
+                    </div>
+                `;
+            }
+            mytemplate = mytemplate + `<button class="navbar-toggler navbar-toggler-right font-weight-bold bg-primary text-white rounded" id="check-answer-button-${topic}" onclick="checkAllAnswers('${topic}', '${numberOfQuestions}')">Check all answers</button>`;
+            document.getElementById(index.toString()).innerHTML = mytemplate;
+        });
+    } else if (option === 'multiple-choice') {
+        mydata.jillsquiz.forEach((element, index) => {
+            let topic = element.topic;
+            // let mytemplate = `<iframe width="280" height="157.5" src='${element.quizquestions[0].video_source}' title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            let mytemplate = ``;
+            let numberOfQuestions = element.quizquestions.length;
+            for (let i = 0; i < element.quizquestions.length; i++) {
+            let ques = element.quizquestions[i].question;
+        
+            let ans = element.quizquestions[i].correct_answer;
+            let ans1 = element.quizquestions[i].answers[0];
+            let ans2 = element.quizquestions[i].answers[1];
+            let ans3 = element.quizquestions[i].answers[2];
+        
+            mytemplate =
+                mytemplate +
+                `<div class="col-lg-12 ml-auto">
+                <p class="pre-wrap lead">Question - ${ques}</p>
+                <p class="pre-wrap lead">
+                    <label for="${topic}">Choose an answer:</label>
+                    <select name="${topic}" id="${topic}-option-${i}">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                    <ol>
+                        <li>${ans1}</li>
+                        <li>${ans2}</li>
+                        <li>${ans3}</li>
+                    </ol>
+                </p>
+                <br/><br/>
+                </div>
+            `;
+            }
+            mytemplate = mytemplate + `<button class="navbar-toggler navbar-toggler-right font-weight-bold bg-primary text-white rounded" id="check-answer-button-${topic}" onclick="checkAllAnswers('${topic}', '${numberOfQuestions}')">Check all answers</button>`;
+            document.getElementById(index.toString()).innerHTML = mytemplate;
+        });
+    } else if (option === 'both') {
+        mydata.jillsquiz.forEach((element, index) => {
+            let topic = element.topic;
+            let mytemplate = `<iframe width="280" height="157.5" src='${element.quizquestions[0].video_source}' title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            let numberOfQuestions = element.quizquestions.length;
+            for (let i = 0; i < element.quizquestions.length; i++) {
+            let ques = element.quizquestions[i].question;
+        
+            let ans = element.quizquestions[i].correct_answer;
+            let ans1 = element.quizquestions[i].answers[0];
+            let ans2 = element.quizquestions[i].answers[1];
+            let ans3 = element.quizquestions[i].answers[2];
+        
+            mytemplate =
+                mytemplate +
+                `<div class="col-lg-12 ml-auto">
+                <p class="pre-wrap lead">Question - ${ques}</p>
+                <p class="pre-wrap lead">
+                    <label for="${topic}">Choose an answer:</label>
+                    <select name="${topic}" id="${topic}-option-${i}">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                    <ol>
+                        <li>${ans1}</li>
+                        <li>${ans2}</li>
+                        <li>${ans3}</li>
+                    </ol>
+                </p>
+                <br/><br/>
+                </div>
+            `;
+            }
+            mytemplate = mytemplate + `<button class="navbar-toggler navbar-toggler-right font-weight-bold bg-primary text-white rounded" id="check-answer-button-${topic}" onclick="checkAllAnswers('${topic}', '${numberOfQuestions}')">Check all answers</button>`;
+            document.getElementById(index.toString()).innerHTML = mytemplate;
+        });
+    }
+}
 function getTotal() {
     alert(`Your total is ${totalScore}`);
 }
