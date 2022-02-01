2aec3c5bcf8a6c56a818b2a1cc52f88f67c14460
Alex Nicholas <alex.nicholas@bbc.co.uk>
2022-02-01 15:20:16 +0000

    v2 good

--- freelancer/quizCheck.js
+++ freelancer/quizCheck.js
@@ -1,8 +1,11 @@
+let totalScore = 0;
 let topicScore = 0;
+let topics = ["celebrities", "conservation", "geography", "history", "law", "politics", "sport"];
 
 mydata.jillsquiz.forEach((element, index) => {
   let topic = element.topic;
   let mytemplate = "";
+  let numberOfQuestions = element.quizquestions.length;
   for (let i = 0; i < element.quizquestions.length; i++) {
     let ques = element.quizquestions[i].question;
 
@@ -17,7 +20,7 @@ mydata.jillsquiz.forEach((element, index) => {
         <p class="pre-wrap lead">Question - ${ques}</p>
         <p class="pre-wrap lead">
             <label for="${topic}">Choose an answer:</label>
-            <select name="${topic}" id="${topic}-option">
+            <select name="${topic}" id="${topic}-option-${i}">
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -28,29 +31,50 @@ mydata.jillsquiz.forEach((element, index) => {
                 <li>${ans3}</li>
             </ol>
         </p>
-        <button onclick="check('${topic}', ${ans})">Check this answer</button>
         <br/><br/>
       </div>
      `;
-     console.log("index", document.getElementById(index.toString()));
   }
-  mytemplate = mytemplate + `<button onclick="checkAllAnswers()">Check all answers</button>`;
+  mytemplate = mytemplate + `<button id="check-answer-button-${topic}" onclick="checkAllAnswers('${topic}', '${numberOfQuestions}')">Check all answers</button>`;
   document.getElementById(index.toString()).innerHTML = mytemplate;
 });
 
-function checkAllAnswers() {
-    alert("Score: ", topicScore);
-//     for (let i = 0; i < element.quizquestions.length; i++) {
+function checkAllAnswers(thistopic, numberOfQuestions) {
+    topicScore = 0;
+    var topicIndex = topics.indexOf(thistopic);
+
+
+
+
+
+    for (let i = 0; i < numberOfQuestions; i++) {
+        let dummy = document.getElementById(`${thistopic}-option-${i}`).value;
+        
+        
+        if (+dummy == +mydata.jillsquiz[topicIndex].quizquestions[i].correct_answer) {
+            //alert("Correct");
+            //add to total
+            topicScore += 1;
+          } else {
+            //alert("Not Correct");
+          }
+    }
+    totalScore += topicScore;
+    document.getElementById(`check-answer-button-${thistopic}`).disabled = true;
+    alert(`Score:  ${topicScore} Total: ${totalScore}`);
 }
 function check(thistopic, correctanswer) {
-  let answervalue = document.getElementById(`${thistopic}-option`).value;
+  let answervalue = document.getElementById(`${thistopic}-option-0`).value;
 
   if (+answervalue == +correctanswer) {
     alert("Correct");
     //add to total
     topicScore += 1;
-    console.log("score: ", topicScore);
   } else {
     alert("Not Correct");
   }
+}
+
+function getTotal() {
+    alert(`Your total is ${totalScore}`);
 }
